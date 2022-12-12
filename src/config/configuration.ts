e4b682f3611368a5f44385be36f3a3ad9a20aa0a
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2022-12-12 18:29:33 -0500

    fix(configuration): set default env variables values to avoid errors

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   database: {
-    host: process.env.DATABASE_HOST,
-    name: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST || "",
+    name: process.env.DATABASE_NAME || "",
   },
 });
