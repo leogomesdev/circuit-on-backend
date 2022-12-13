5ba3ad8f2324caab6b6337163075ee4abdfe3aba
Leonardo Gomes da Silva <leogomesdev@gmail.com>
2022-12-12 20:22:43 -0500

    fix(configuration): backward compatibility for database connection env

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,6 @@
 export default () => ({
   database: {
-    host: process.env.DATABASE_HOST || "",
-    name: process.env.DATABASE_NAME || "",
+    host: process.env.DATABASE_HOST || process.env.MONGODB_DB_URI || '',
+    name: process.env.DATABASE_NAME || 'propaganda-app',
   },
 });
